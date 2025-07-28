786227be47dbd5a57a5e2374922331aefe4909a3
mrboxs <rizkywanda200155@gmail.com>
2025-07-29 04:29:39 +0700

    feat: add custom logging middleware and error handling
    - Introduced a custom logger using pino for structured logging.
    - Added middleware for handling not found errors and general error responses.
    - Implemented a middleware to serve an emoji favicon.
    - Updated ESLint configuration to disable specific style rules.
    - Added new dependencies for logging and date manipulation.
    - Refactored app creation to include new middlewares and error handling.
    - Added HTTP status codes and phrases for better response management.

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -1,7 +1,16 @@
+import { prettyJSON } from 'hono/pretty-json';
+
 import factory from '@/lib/factory';
+import { logger, notFound, onError, serveEmojiFavicon } from '@/middlewares';
 
 export default function createApp() {
   const app = factory.createApp();
+  app.use('*', logger());
+  app.use('*', prettyJSON());
+  app.use('*', serveEmojiFavicon('🔥'));
+
+  app.onError(onError);
+  app.notFound(notFound);
 
   return app;
 }
---
+++ src/middlewares/logger.ts
@@ -0,0 +1,30 @@
+import { format } from 'date-fns';
+import { pinoLogger } from 'hono-pino';
+import pino from 'pino';
+import pinoPretty from 'pino-pretty';
+
+import { env } from '@/utils/env';
+
+export function logger() {
+  return pinoLogger({
+    pino: pino(
+      {
+        level: env.LOG_LEVEL,
+      },
+      pinoPretty()
+    ),
+    http: {
+      reqId: () => crypto.randomUUID(),
+    },
+  });
+}
+
+const formatTime = () => format(new Date(), 'HH:mm:ss');
+
+export const cusLog = pino(
+  {
+    base: { pid: false },
+    timestamp: () => `,"time":"${formatTime()}"`,
+  },
+  pinoPretty()
+);
